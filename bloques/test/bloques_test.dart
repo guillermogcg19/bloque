e77d5e15245de9d781223269d777a14bda9eef0c
Guillermo Carrillo <l22440589@mochis.tecnm.mx>
2025-10-06 14:06:07 -0700

    Implementacion de tests para el algoritmo de bloques - Carriilo

--- bloques/test/bloques_test.dart
+++ bloques/test/bloques_test.dart
@@ -2,6 +2,18 @@ import 'package:test/test.dart';
 import 'package:bloques/bloques.dart';
 
 void main() {
- 
- 
+  group('primer ejemplo', () {
+    test('ejemplo 1', () => expect(bloques([6, 0, 3, 4, 1, 3]), 7));
+  });
+
+  group('normales', () {
+    test('normal 1', () => expect(bloques([0,1,0,2,1,0,1,3,2,1,2,1]), 6));
+    test('normal 2', () => expect(bloques([4,2,0,3,2,5]), 9));
+  });
+
+  group('inesperados', () {
+    test('vacio', () => expect(bloques([]), 0));
+    test('uno', () => expect(bloques([5]), 0));
+    test('todos iguales', () => expect(bloques([2,2,2]), 0));
+  });
 }
